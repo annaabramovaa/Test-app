1440091523b8da0461689282611f73418caee115
annaabramovaa <93001839+annaabramovaa@users.noreply.github.com>
2024-05-19 13:09:48 +0300

    Create the appearance of the site

---
+++ src/Components/Events.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./Events.css";
+
+function Events() {
+  return (
+    <div className="events">
+      <div className="event-box">
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+        <div className="event-1">
+          <h4>Title</h4>
+          <p>description</p>
+          <div className="buttons-container">
+            <button className="register">Sign Up</button>
+            <button className="participants">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Events;
---
+++ src/Components/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Registration.css";
+
+function Registration() {
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+}
+
+export default Registration;
